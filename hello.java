f8e978c3a7c36b5157a066816b85fcf2895f46f4
George Van Thiel Berghuijs <83961366+georgewvtb@users.noreply.github.com>
2021-05-26 21:32:28 +0200

    Made a change

--- hello.java
+++ hello.java
@@ -1,5 +1,7 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Brady");
+        System.Out.println("Hello Brady, my name is George.")
     }
+
 }
